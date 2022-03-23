ddabe76673a6e257e94d18c265c6d008a596a1a9
PapaFreddie <fred.papa@student.moringaschool.com>
2022-03-23 13:12:44 +0300

    update all

--- week3/tests.py
+++ week3/tests.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 class ProfileTestClass(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create_user('amos')
+        cls.user = User.objects.create_user('fred')
         cls.profile1 = Profile(profile_pics = 'bab.jpg', bio = 'born in Kenya', user = cls.user)
 
     def test_instance(cls):
@@ -22,7 +22,7 @@ class ImageTestClass(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up data for the whole TestCase
-        cls.user = User.objects.create_user('amos')
+        cls.user = User.objects.create_user('fred')
         cls.new_profile = Profile(profile_pics = 'bab.jpg', bio = 'born in Kenya', user = cls.user)
         cls.new_image = Image(my_image='/static/images/', caption='Corona', profile=cls.new_profile)
 
